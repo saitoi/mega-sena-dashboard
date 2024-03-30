3dedb2a3c5ba99bf7404aa9a64ec41c2c59e3b2c
saitoi <pedrohhs@dcc.ufrj.br>
2024-03-30 00:47:47 -0300

    Adding callbacks file

--- app/dashapp/callbacks.py
+++ app/dashapp/callbacks.py
@@ -1,18 +1,29 @@
-from dash.dependencies import Input, Output, State
-import dash
+from dash.dependencies import Input, Output, State, ALL
+from dash import callback_context
 
-def register_callbacks(app: dash.Dash):
+def register_callbacks(app):
     @app.callback(
         Output('pressed-buttons-store', 'data'),
-        [Input({'type': 'button', 'index': all}, 'n_clicks')],
-        [State('pressed-buttons-store', 'data')]
+        Input({'type': 'button', 'index': ALL}, 'n_clicks'),
+        State('pressed-buttons-store', 'data')
     )
-    def update_pressed_buttons(n_clicks, pressed_buttons):
-        ctx = dash.callback_context
-        if not ctx.triggered:
-            button_id = 'No buttons yet'
-        else:
-            button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        if button_id not in pressed_buttons:
-            pressed_buttons.append(button_id)
-        return pressed_buttons
+    def update_pressed_buttons(n_clicks, pressed_buttons_data):
+        if pressed_buttons_data is None:
+            pressed_buttons_data = []
+
+        if callback_context.triggered:
+            button_id = callback_context.triggered[0]['prop_id'].split('.')[0]
+
+            if button_id in pressed_buttons_data:
+                pressed_buttons_data.remove(button_id)
+            else:
+                pressed_buttons_data.append(button_id)
+
+        return pressed_buttons_data
+
+    @app.callback(
+        # Suponha que aqui você definiria os Inputs, Outputs e States para o segundo callback
+    )
+    def calcular_ficha_frequencia():
+        pass
+        # Sua lógica aqui
--- app/dashapp/components/mega_sena_token.py
+++ app/dashapp/components/mega_sena_token.py
@@ -1,4 +1,4 @@
-from dash import html, dcc, Input, Output, State
+from dash import html, dcc, Input, Output, State, callback_context
 from dash.dependencies import ALL
 import dash_table
 
@@ -11,8 +11,8 @@ def criar_botoes_ficha(tipo: str, inicio: int, fim: int, intervalo: int=1) -> li
     :param fim: Um inteiro que define o número final da sequência de botões.
     :return: Uma lista contendo os botões criados.
     """
-    return [html.Button(f'[{tipo}]',
-                        id=f'button-{tipo}-{i}',
+    return [html.Button(f'[{i}]',
+                        id=f'{tipo}-{i}',
                         className='dash-buttons') for i in range(inicio, fim+1, intervalo)]
 
 
@@ -22,8 +22,7 @@ def secao_jogo(numero: int) -> list:
                 html.P(f'Tabela {numero}'),
                 criar_botoes_ficha(f'dezena_{numero}', 1, 60),
                 html.P('Para anular este jogo marque: '),
-                html.Button('[  ]', id=f'anular-{numero}', className='dash-buttons')
-                criar_botoes_ficha()
+                html.Button('[  ]', id=f'anular-{numero}', className='dash-buttons'),
             ])
     
 
@@ -47,27 +46,8 @@ ficha = html.Div(
     dcc.Store(id='pressed-buttons-store', data=[])
 )
 
-# ***************** PRECISO MUDAR ABAIXO *****************
+# ***************** CALLBACKS IN "callbacks.py" *****************
 
-@app.callback(
-    Output('pressed-buttons-store', 'data'),
-    [Input({'type': 'button', 'index': ALL}, 'n_clicks')],
-    [State('pressed-buttons-store', 'data')]
-)
-def update_pressed_buttons(n_clicks, pressed_buttons):
-    ctx = dash.callback_context
-
-    if not ctx.triggered:
-        button_id = 'No buttons yet'
-    else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        # The ID is in a string format like '{"type":"button","index":"button-dezena_1-1"}'
-        # You might need to parse this string to extract the exact button ID in your desired format
-
-    if button_id not in pressed_buttons:
-        pressed_buttons.append(button_id)
-
-    return pressed_buttons
 
 # ***************** TABELA CORRESPONDENTE *****************
 
